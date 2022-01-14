555d546a87ebfb1d69a0dea4c17d23402e70b18d
pengpengc <pp@12345678>
2022-01-14 18:01:03 +0800

    commit

--- xmodule3-common/xmodule3-common-core/src/main/java/com/penglecode/xmodule/common/config/DefaultSwaggerConfiguration.java
+++ xmodule3-common/xmodule3-common-core/src/main/java/com/penglecode/xmodule/common/config/DefaultSwaggerConfiguration.java
@@ -1,59 +1,76 @@
 package com.penglecode.xmodule.common.config;
 
-import com.github.xiaoymin.knife4j.spring.annotations.EnableKnife4j;
-import io.swagger.v3.oas.annotations.Operation;
+import com.penglecode.xmodule.BasePackage;
+import io.swagger.v3.oas.models.ExternalDocumentation;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import io.swagger.v3.oas.models.servers.Server;
+import org.springdoc.core.GroupedOpenApi;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import springfox.documentation.builders.ApiInfoBuilder;
-import springfox.documentation.builders.PathSelectors;
-import springfox.documentation.builders.RequestHandlerSelectors;
-import springfox.documentation.oas.annotations.EnableOpenApi;
-import springfox.documentation.service.ApiInfo;
-import springfox.documentation.service.Contact;
-import springfox.documentation.spi.DocumentationType;
-import springfox.documentation.spring.web.plugins.Docket;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.Collections;
+import java.util.List;
 
 /**
- * 默认的swagger配置
+ * 默认的swagger配置(基于springdoc)
  *
  * @author pengpeng
  * @version 1.0
  * @since 2021/4/13 23:01
  */
 @Configuration
-@EnableOpenApi
-@EnableSwagger2
-@EnableKnife4j
 public class DefaultSwaggerConfiguration extends AbstractSpringConfiguration {
 
-    private final SwaggerConfigProperties swaggerConfigProperties;
+    /**
+     * 不分组的OpenAPI
+     */
+    /*@Bean
+    public OpenAPI defaultNonGroupOpenApi() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("测试API文档")
+                        .description(String.format("%s接口测试文档", getEnvironment().getProperty("spring.application.name")))
+                        .version(getEnvironment().getProperty("spring.application.version", "1.0.0"))
+                        .contact(new Contact().name("Pengle").url("https://github.com/penggle").email("pengpeng.prox@gmail.com"))
+                        .license(new License().name("Apache2.0").url("https://springdoc.org")))
+                .externalDocs(new ExternalDocumentation()
+                        .description("OpenAPI规范参考文档")
+                        .url("https://github.com/OAI/OpenAPI-Specification/blob/3.0.1/versions/3.0.1.md"));
+    }*/
 
-    public DefaultSwaggerConfiguration(SwaggerConfigProperties swaggerConfigProperties) {
-        this.swaggerConfigProperties = swaggerConfigProperties;
-    }
-
+    /**
+     * 使用分组的OpenAPI，将所有API放在一个默认大组中，解决knife4j-springdoc-ui:3.0.3报错的问题
+     * PS: 如果不分组的话则申明{@link OpenAPI} bean
+     */
     @Bean
-    public Docket createRestApi() {
-        return new Docket(DocumentationType.OAS_30)
-                .enable(swaggerConfigProperties.isEnabled())
-                .useDefaultResponseMessages(false)
-                .apiInfo(apiInfo())
-                .select()
-                .apis(RequestHandlerSelectors.basePackage(swaggerConfigProperties.getBasePackage())
-                        .and(RequestHandlerSelectors.withMethodAnnotation(Operation.class)))
-                .paths(PathSelectors.any())
+    public GroupedOpenApi defaultGroupOpenApi() {
+        return GroupedOpenApi.builder().group("default")
+                .addOpenApiCustomiser(openApi -> openApi.info(info()).servers(servers()).setExternalDocs(externalDocs()))
+                .packagesToScan(BasePackage.class.getPackage().getName())
+                .pathsToMatch("/api/**")
                 .build();
-
     }
 
-    private ApiInfo apiInfo() {
-        return new ApiInfoBuilder()
+    private Info info() {
+        return new Info()
+                .title("测试API文档")
                 .description(String.format("%s接口测试文档", getEnvironment().getProperty("spring.application.name")))
-                .contact(new Contact("Pengle", "https://github.com/penggle", "pengpeng.prox@gmail.com"))
                 .version(getEnvironment().getProperty("spring.application.version", "1.0.0"))
-                .title("API测试文档")
-                .build();
+                .contact(new Contact().name("Pengle").url("https://github.com/penggle").email("pengpeng.prox@gmail.com"))
+                .license(new License().name("Apache2.0").url("https://springdoc.org"));
+    }
+
+    private ExternalDocumentation externalDocs() {
+        return new ExternalDocumentation()
+                .description("OpenAPI规范参考文档")
+                .url("https://github.com/OAI/OpenAPI-Specification/blob/3.0.1/versions/3.0.1.md");
+    }
+
+    private List<Server> servers() {
+        return Collections.singletonList(new Server().url(String.format("http://127.0.0.1:%s", getEnvironment().getProperty("server.port", "8080"))).description("Embedded Tomcat9"));
     }
 
 }
