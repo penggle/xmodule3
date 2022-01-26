7423f18d25100f3aa4e35aa72d838d65f1886464
penggle <pengpeng.prox@gmail.com>
2022-01-26 17:54:55 +0800

    commit

--- xmodule3-common/xmodule3-common-codegen2/src/main/java/com/penglecode/xmodule/common/codegen/support/TemplateParameter.java
+++ xmodule3-common/xmodule3-common-codegen2/src/main/java/com/penglecode/xmodule/common/codegen/support/TemplateParameter.java
@@ -1,6 +1,7 @@
 package com.penglecode.xmodule.common.codegen.support;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 /**
@@ -10,6 +11,7 @@ import java.util.Map;
  * @version 1.0
  * @since 2021/1/24 15:50
  */
+@SuppressWarnings("unchecked")
 public class TemplateParameter extends HashMap<String,Object> {
 
     private static final long serialVersionUID = 1L;
@@ -26,6 +28,7 @@ public class TemplateParameter extends HashMap<String,Object> {
         put("templateFileName", templateFileName);
     }
 
+    /** 待生成目标对象的Freemarker模板文件名 */
     public String getTemplateFileName() {
         return (String) get("templateFileName");
     }
@@ -34,40 +37,117 @@ public class TemplateParameter extends HashMap<String,Object> {
         put("targetFileName", targetFileName);
     }
 
+    /** 待生成目标对象的文件名 */
     public String getTargetFileName() {
         return (String) get("targetFileName");
     }
 
-    public void setTargetObjectName(String targetObjectName) {
-        put("targetObjectName", targetObjectName);
+    public void setTargetProjectImports(List<String> targetProjectImports) {
+        put("targetProjectImports", targetProjectImports);
     }
 
-    public String getTargetObjectName() {
-        return (String) get("targetObjectName");
+    /** 待生成目标对象的项目内imports */
+    public List<String> getTargetProjectImports() {
+        return (List<String>) get("targetProjectImports");
+    }
+
+    public void setTargetThirdImports(List<String> targetThirdImports) {
+        put("targetThirdImports", targetThirdImports);
+    }
+
+    /** 待生成目标对象的第三方imports */
+    public List<String> getTargetThirdImports() {
+        return (List<String>) get("targetThirdImports");
+    }
+
+    public void setTargetJdkImports(List<String> targetJdkImports) {
+        put("targetJdkImports", targetJdkImports);
+    }
+
+    /** 待生成目标对象的JDK imports */
+    public List<String> getTargetJdkImports() {
+        return (List<String>) get("targetJdkImports");
+    }
+
+    public void setTargetClass(String targetClass) {
+        put("targetClass", targetClass);
+    }
+
+    /** 待生成目标对象的类名 */
+    public String getTargetClass() {
+        return (String) get("targetClass");
+    }
+
+    public void setTargetExtends(String targetExtends) {
+        put("targetExtends", targetExtends);
+    }
+
+    /** 待生成目标对象的extends父类名 */
+    public String getTargetExtends() {
+        return (String) get("targetExtends");
+    }
+
+    public void setTargetImplements(List<String> targetImplements) {
+        put("targetImplements", targetImplements);
+    }
+
+    /** 待生成目标对象的实现接口名列表 */
+    public List<String> getTargetImplements() {
+        return (List<String>) get("targetImplements");
     }
 
     public void setTargetPackage(String targetPackage) {
         put("targetPackage", targetPackage);
     }
 
+    /** 待生成目标对象的包名 */
     public String getTargetPackage() {
         return (String) get("targetPackage");
     }
 
-    public void setCommentAuthor(String commentAuthor) {
-        put("commentAuthor", commentAuthor);
+    public void setTargetComment(String targetComment) {
+        put("targetComment", targetComment);
     }
 
-    public String getCommentAuthor() {
-        return (String) get("commentAuthor");
+    /** 待生成目标对象的注释 */
+    public String getTargetComment() {
+        return (String) get("targetComment");
     }
 
-    public void setGenerateTime(String generateTime) {
-        put("generateTime", generateTime);
+    public void setTargetAuthor(String targetAuthor) {
+        put("targetAuthor", targetAuthor);
     }
 
-    public String getGenerateTime() {
-        return (String) get("generateTime");
+    /** 待生成目标对象的作者 */
+    public String getTargetAuthor() {
+        return (String) get("targetAuthor");
+    }
+
+    public void setTargetVersion(String targetVersion) {
+        put("targetVersion", targetVersion);
+    }
+
+    /** 待生成目标对象的版本 */
+    public String getTargetVersion() {
+        return (String) get("targetVersion");
+    }
+
+    public void setTargetCreated(String targetCreated) {
+        put("targetCreated", targetCreated);
+    }
+
+    /** 待生成目标对象的创建时间 */
+    public String getTargetCreated() {
+        return (String) get("targetCreated");
+    }
+
+    public void setTargetAnnotations(List<String> targetAnnotations) {
+        put("targetAnnotations", targetAnnotations);
+    }
+
+    /** 待生成目标对象的SpringBean的名称 */
+    public List<String> getTargetAnnotations() {
+        return (List<String>) get("targetAnnotations");
     }
 
     public void setDomainObjectTitle(String domainObjectTitle) {
